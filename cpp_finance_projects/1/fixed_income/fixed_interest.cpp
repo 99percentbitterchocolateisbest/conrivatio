ca54343571e8763b1ffc795cf9be006112a4e9f1
99percentbitterchocolateisbest <117770578+99percentbitterchocolateisbest@users.noreply.github.com>
2025-03-17 11:19:54 +0300

    bugfix

--- cpp_finance_projects/1/fixed_income/fixed_interest.cpp
+++ cpp_finance_projects/1/fixed_income/fixed_interest.cpp
@@ -7,7 +7,7 @@ public:
   if(&v != this) this->m_rate = v.m_rate;
   return *this;
   }
-  ~IntRate();
+  ~IntRate(){};
   inline double single_period(double value){
     return value * (1 + m_rate);
   }
@@ -17,7 +17,7 @@ private:
 
 
 int main (int argc, const char * argv[]) {
-  if(argc != 3) std::cout<< "Usage: <program> <interest_rate> <value>" << std::endl;
+  if(argc != 3){  std::cout<< "Usage: <program> <interest_rate> <value>" << std::endl; return 1; }
 
   IntRate x(atof(argv[1]));
   std::cout << x.single_period(atof(argv[2]));
