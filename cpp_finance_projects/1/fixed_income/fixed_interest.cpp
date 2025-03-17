d40b20355b7ace3d00710d9e43679f4a878ae52f
99percentbitterchocolateisbest <117770578+99percentbitterchocolateisbest@users.noreply.github.com>
2025-03-17 11:11:46 +0300

    fixed interest

---
+++ cpp_finance_projects/1/fixed_income/fixed_interest.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+class IntRate{
+public:
+  IntRate(double rate) : m_rate(rate){}
+  IntRate(const IntRate &v): m_rate(v.m_rate){}
+  IntRate &operator =(const IntRate &v){
+  if(&v != this) this->m_rate = v.m_rate;
+  return *this;
+  }
+  ~IntRate();
+  inline double single_period(double value){
+    return value * (1 + m_rate);
+  }
+private:
+  double m_rate;
+};
+
+
+int main (int argc, const char * argv[]) {
+  if(argc != 3) std::cout<< "Usage: <program> <interest_rate> <value>" << std::endl;
+
+  IntRate x(atof(argv[1]));
+  std::cout << x.single_period(atof(argv[2]));
+
+  return 0;
+}
