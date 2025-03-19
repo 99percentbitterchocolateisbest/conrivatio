8a90e550933c65f218003ec55bfd06ea96ff7bce
99percentbitterchocolateisbest <117770578+99percentbitterchocolateisbest@users.noreply.github.com>
2025-03-19 20:22:11 +0300

    compound interest

--- cpp_finance_projects/1/fixed_income/compound_interest.cpp
+++ cpp_finance_projects/1/fixed_income/compound_interest.cpp
@@ -1,4 +1,46 @@
+/*
+ *  V=P(1+R)^2
+ *  V=Pe^(RN)
+ */
+#include <iostream>
+#include <cmath>
+
+class cmp{
+  double m_rate;
+  public:
+  cmp(double rate): m_rate(rate) {}
+  cmp &operator =(const cmp &v){
+     if(this != &v) this->m_rate = v.m_rate;
+    return *this;
+  }
+  cmp(const cmp  &v) : m_rate(v.m_rate) {}
+  ~cmp(){}
+  double multiplePeriod(double value, int numPeriods){
+    return value * pow(1 + m_rate, numPeriods);
+  }
+  double continuousCompounding(double value, int numPeriods){
+    return value * exp(m_rate * numPeriods);
+  }
+
+
+};
+
+
+
 int main (int argc, char *argv[]) {
+  if (argc != 4){std::cout << "usage: progName <interest rate> <present value> <numperiods>" << std::endl;
+  return 1;
+}
+  double rate = atof(argv[1]);
+  double value = atof(argv[2]);
+  int num_periods = atoi(argv[3]);
+
+  cmp x(rate);
   
+  
+ std::cout << " future value for multiple period compounding is " << x.multiplePeriod(value, num_periods) << std::endl;
+
+ std::cout << " future value for continuous compounding is " << x.continuousCompounding(value, num_periods)<< std::endl;
+
   return 0;
 }
